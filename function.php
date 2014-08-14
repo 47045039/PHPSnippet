9a15034afe91082a249a10954c6b442026e7344e
mts <mountains.liu@gmail.com>
2014-08-14 19:15:50 +0800

    Test functions 2.

--- function.php
+++ function.php
@@ -15,7 +15,7 @@ if (false) {
 	bar (); // function bar()
 }
 
-if (true) {
+if (false) {
 	// array作为参数
 	function takes_array($input) {
 		echo "$input[0] + $input[1] = ", $input [0] + $input [1]; // 1 + 2 = 3
@@ -51,22 +51,80 @@ if (true) {
 	echo makecoffee ( "espresso" ); // Making a cup of espresso.
 }
 
-if (true) {
-	// func_num_args() 	获取函数参数个数
-	// func_get_args()	获取函数所有参数
-	// func_get_arg ()	获取函数的某一个参数，索引从0开始
+if (false) {
+	// func_num_args() 获取函数参数个数
+	// func_get_args() 获取函数所有参数
+	// func_get_arg () 获取函数的某一个参数，索引从0开始
 	function foo() {
-		$numargs = func_num_args ();  // 3
+		$numargs = func_num_args (); // 3
 		echo "Number of arguments: $numargs<br />\n";
 		if ($numargs >= 2) {
 			echo "Second argument is: " . func_get_arg ( 1 ) . "<br />\n"; // 2
 		}
 		$arg_list = func_get_args ();
 		for($i = 0; $i < $numargs; $i ++) {
-			echo "Argument $i is: " . $arg_list [$i] . "<br />\n";	// 1, 2, 3
+			echo "Argument $i is: " . $arg_list [$i] . "<br />\n"; // 1, 2, 3
 		}
 	}
 	
 	foo ( 1, 2, 3 );
 }
+
+if (false) {
+	function foo() {
+		echo "In foo()<br>\n";
+	}
+	function bar($arg = '') {
+		echo "In bar(); argument was '$arg'.<br>\n";
+	}
+	function echoit($string) {
+		echo $string;
+	}
+	
+	$func = 'foo';
+	$func (); // 变量函数，实际调用foo()
+	
+	$func = 'bar';
+	$func ( 'test' ); // 变量函数，实际调用bar()
+	
+	$func = 'echoit';
+	$func ( 'test' ); // 变量函数，实际调用echoit()
+	class Foo {
+		function Varr() {
+			echo "This is " . __FUNCTION__; // This is Varr
+			$name = 'Bar';
+			$this->$name (); // 调用Bar()
+		}
+		function Bar() {
+			echo "This is " . __METHOD__; // This is Foo::Bar
+		}
+	}
+	
+	$foo = new Foo ();
+	$funcname = "Varr";
+	$foo->$funcname (); // 调用Varr()，然后调用Bar()
+}
+
+if (true) {
+	function test() {
+	}
+	function test1() {
+	}
+	echo function_exists ( 'test' );
+	
+	// array(2) {
+	// ["internal"]=>array(1765) {...}
+	// ["internal"]=>array(2) {[0] => string(4) "test", [1] => string(5) "test1"}
+	// }
+	
+	var_dump ( get_defined_functions () );
+	
+	// array(24) {
+	// [0] => string(17) "xml_parser_create"
+	// [1] => string(20)
+	// ....
+	// }
+	var_dump ( get_extension_funcs ( 'xml' ) );
+}
+
 ?>
